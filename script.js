881a2fa65add6742cd725f09f7a59096406fd64c
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-19 18:10:51 +0100

    html aggiornato

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo while.
+
+const groceryList = [
+    'Pane',
+    'Latte',
+    'Uova',
+    'Frutta',
+    'Verdura',
+    'Pasta',
+    'Riso',
+    'Carne',
+    'Pesce',
+    'Formaggio',
+    'Sugo',
+    'Olio',
+    'Sale',
+    'Zucchero',
+    'Caffè',
+    'Te',
+    'Biscotti',
+    'Cioccolato',
+    'Yogurt',
+    'Cereali',
+    'Sapone',
+    'Shampoo',
+    'Dentifricio',
+    'Carta igienica'
+];
+
+let index = 0;
+
+while (index < groceryList.length) {
+    
+    ((index + 1) + '. ' + groceryList[index]);
+    index++;
+}
